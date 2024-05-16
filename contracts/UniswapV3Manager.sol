402fe3f72796f46dd3a8de72b8c2de1402f4c0dc
peizeng <soygaga529@gmail.com>
2024-05-17 03:34:28 +0800

    fix manager exceeding max code size

--- contracts/UniswapV3Manager.sol
+++ contracts/UniswapV3Manager.sol
@@ -8,7 +8,6 @@ import "./lib/TickMath.sol";
 import "./lib/LiquidityMath.sol";
 import "./lib/PoolAddress.sol";
 import "./lib/Path.sol";
-import "hardhat/console.sol";
 
 contract UniswapV3Manager is IUniswapV3Manager {
     using Path for bytes;
@@ -30,7 +29,6 @@ contract UniswapV3Manager is IUniswapV3Manager {
             params.fee
         );
         IUniswapV3Pool pool = IUniswapV3Pool(poolAddress);
-        console.log("poolAddress %s", poolAddress);
 
         (uint160 sqrtPriceX96,,,,) = pool.slot0();
         uint160 sqrtPriceLowerX96 = TickMath.getSqrtRatioAtTick(params.lowerTick);
@@ -79,38 +77,6 @@ contract UniswapV3Manager is IUniswapV3Manager {
         );
     }
 
-    function swap(SwapParams memory params) public returns (uint256 amountOut) {
-        address payerAddress = msg.sender;
-        bool hasMultiplePools;
-
-        while (true) {
-            hasMultiplePools = params.path.hasMultiplePools();
-            address actualRecipient = hasMultiplePools ? address(this) : params.recipient;
-
-            params.amountIn = _swap(
-                params.amountIn,
-                actualRecipient,
-                0,
-                SwapCallbackData({
-                    path: params.path.getFirstPool(),
-                    payerAddress: payerAddress
-                })
-            );
-
-            if (hasMultiplePools) {
-                payerAddress = address(this);
-                params.path = params.path.skipToken();
-            } else {
-                amountOut = params.amountIn;
-                break;
-            }
-        }
-
-        if (amountOut < params.minAmountOut) {
-            revert TooLittleReceived(amountOut);
-        }
-    }
-
     function _swap(
         uint256 amountIn,
         address recipient,
@@ -127,9 +93,6 @@ contract UniswapV3Manager is IUniswapV3Manager {
                 sqrtPriceLimit = TickMath.MAX_SQRT_RATIO - 1;
             }
         }
-        console.log("tokenInAddress %s", tokenInAddress);
-        console.log("tokenOutAddress %s", tokenOutAddress);
-        console.log("fee %s", fee);
         (int256 amount0, int256 amount1) = getPool(tokenInAddress, tokenOutAddress, fee).swap(
             recipient,
             zeroForOne,
@@ -148,34 +111,19 @@ contract UniswapV3Manager is IUniswapV3Manager {
         (token0Address, token1Address) = token0Address < token1Address
             ? (token0Address, token1Address) : (token1Address, token0Address);
         address poolAddress = PoolAddress.computePoolAddress(factoryAddress, token0Address, token1Address, fee);
-        console.log("poolAddress2 %s", poolAddress);
         pool = IUniswapV3Pool(poolAddress);
     }
 
-    function flash(
-        address poolAddress,
-        uint256 amount0,
-        uint256 amount1,
-        bytes calldata data
-    ) public {
-        return IUniswapV3Pool(poolAddress).flash(amount0, amount1, data);
-    }
-
-    // 用户amount0和amount1给池子
     function uniswapV3MintCallback(
         uint256 amount0,
         uint256 amount1,
         bytes calldata data
     ) public {
         IUniswapV3Pool.CallbackData memory callbackData = abi.decode(data, (IUniswapV3Pool.CallbackData));
-//        console.log("TransferFrom token0: %s, sender: %s, receiver: %s",
-//            callbackData.token0, callbackData.payer, msg.sender);
-//        console.log("amount0: %s", amount0);
         IERC20(callbackData.token0).transferFrom(callbackData.payer, msg.sender, amount0);
         IERC20(callbackData.token1).transferFrom(callbackData.payer, msg.sender, amount1);
     }
 
-    // 用户要卖amount0就只转amount0给池子, 要卖amount1就只转amount1给池子，二选一
     function uniswapV3SwapCallback(
         int256 amount0,
         int256 amount1,
@@ -190,20 +138,6 @@ contract UniswapV3Manager is IUniswapV3Manager {
             IERC20(tokenInAddress).transfer(msg.sender, uint256(amount));
         } else {
             IERC20(tokenInAddress).transferFrom(callbackData.payerAddress, msg.sender, uint256(amount));
-        }
-    }
-
-    function uniswapV3FlashCallback(uint256 fee0, uint256 fee1, bytes calldata data) public {
-        (address token0, address token1, uint256 amount0, uint256 amount1) = abi.decode(data,
-            (address, address, uint256, uint256));
-
-        // todo do something
-
-        if (amount0 > 0) {
-            IERC20(token0).transfer(msg.sender, amount0 + fee0);
-        }
-        if (amount1 > 0) {
-            IERC20(token1).transfer(msg.sender, amount1 + fee1);
         }
     }
 
