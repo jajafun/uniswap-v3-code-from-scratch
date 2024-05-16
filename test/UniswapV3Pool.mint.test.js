e0d430f0611259e796427ca9d5a97e561bb6073b
peizeng <soygaga529@gmail.com>
2024-05-16 21:57:28 +0800

    test mint

--- test/UniswapV3Pool.mint.test.js
+++ test/UniswapV3Pool.mint.test.js
@@ -1,10 +1,13 @@
 const {assert, expect} = require('chai');
 const {ethers} = require('hardhat');
+const {min} = require("hardhat/internal/util/bigint");
 const {parseEther, AbiCoder, keccak256, solidityPacked} = ethers;
 
-let signers, accounts, ownerSigner, ownerAccount, lpSigner, lpAccount;
-let token0, token1, token0Account, token1Account;
-let testUtils, pool, poolAccount, manager, managerAccount;
+let signers, accounts, ownerSigner, ownerAddress, lpSigner, lpAddress;
+let weth, usdc, wethAddress, usdcAddress;
+let testUtils, factory, factoryAddress, wethUsdcPool, wethUsdcPoolAddress, pool, poolAddress, manager, managerAddress;
+
+const poolArtifactLocation = "./artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json";
 
 describe("UniswapV3Pool mint tests", function () {
     before(async () => {
@@ -18,6 +21,8 @@ describe("UniswapV3Pool mint tests", function () {
         let currentPrice = "5000";
         let poolBalance;
         const mintParamsArr = [];
+        const expectedAmount0 = parseEther("0.987492179736600509");
+        const expectedAmount1 = parseEther("4999.999999999999999999");
         before(async function () {
             const mintParams0 = await mintParams(4545, 5500, "1", currentPrice);
             mintParamsArr.push(mintParams0);
@@ -32,24 +37,66 @@ describe("UniswapV3Pool mint tests", function () {
         })
 
         it("pool balances", function () {
-            assert.equal(poolBalance.poolBalance0, parseEther("0.998995580131581600"));
-            assert.equal(poolBalance.poolBalance1, parseEther("4999.999999999999999999"))
+            assert.equal(poolBalance.poolBalance0, expectedAmount0);
+            assert.equal(poolBalance.poolBalance1, expectedAmount1);
         });
 
         it("mint state", async function () {
-            await assetMintState({
-                pool,
-                token0,
-                token1
-            },{
+            await assertPoolState({
+                pool: wethUsdcPool
+            }, {
+                sqrtPriceX96: await testUtils.sqrtP(currentPrice),
+                tick: await testUtils.tick(currentPrice),
+                fees: [0, 0]
+            });
+
+            await assertBalances({
+                token0: weth,
+                token1: usdc
+            }, {
+                userBalance0: parseEther("1") - expectedAmount0,
+                userBalance1: parseEther("5000") - expectedAmount1,
                 poolBalance0: poolBalance.poolBalance0,
-                poolBalance1: poolBalance.poolBalance1,
+                poolBalance1: poolBalance.poolBalance1
+            });
+
+            await assertPosition({
+                pool: wethUsdcPool
+            }, {
                 lowerTick: mintParamsArr[0].lowerTick,
                 upperTick: mintParamsArr[0].upperTick,
-                positionLiquidity: await liquidity(mintParamsArr[0], currentPrice),
-                currentLiquidity: await liquidity(mintParamsArr[0], currentPrice),
-                sqrtPriceX96: await testUtils.sqrtP(currentPrice),
-                tick: await testUtils.tick(currentPrice)
+                liquidity: await liquidity(mintParamsArr[0], currentPrice),
+                feeGrowthInside0LastX128: 0,
+                feeGrowthInside1LastX128: 0,
+                tokensOwed0: 0,
+                tokensOwed1: 0
+            });
+
+            await assertTick({
+                pool: wethUsdcPool,
+            }, {
+                tick: mintParamsArr[0].lowerTick,
+                initialized: true,
+                liquidityGross: await liquidity(mintParamsArr[0], currentPrice),
+                liquidityNet: await liquidity(mintParamsArr[0], currentPrice)
+            })
+
+            await assertTick({
+                pool: wethUsdcPool,
+            }, {
+                tick: mintParamsArr[0].upperTick,
+                initialized: true,
+                liquidityGross: await liquidity(mintParamsArr[0], currentPrice),
+                liquidityNet: -(await liquidity(mintParamsArr[0], currentPrice))
+            })
+
+            await assertObservation({
+                pool: wethUsdcPool
+            }, {
+                index: 0,
+                timestamp: 1,
+                tickCumulative: 0,
+                initialized: true
             })
         })
     })
@@ -71,7 +118,6 @@ describe("UniswapV3Pool mint tests", function () {
                 mintParamsArr,
             }
             poolBalance = await setup(setupParams);
-            console.log(poolBalance)
         })
 
         it("pool balances", function () {
@@ -80,10 +126,10 @@ describe("UniswapV3Pool mint tests", function () {
         });
 
         it("mint state", async function () {
-            await assetMintState({
+            await assertMany({
                 pool,
-                token0,
-                token1
+                token0: weth,
+                token1: usdc
             },{
                 poolBalance0: poolBalance.poolBalance0,
                 poolBalance1: poolBalance.poolBalance1,
@@ -114,7 +160,6 @@ describe("UniswapV3Pool mint tests", function () {
                 mintParamsArr,
             }
             poolBalance = await setup(setupParams);
-            console.log(poolBalance)
         })
 
         it("pool balances", function () {
@@ -123,10 +168,10 @@ describe("UniswapV3Pool mint tests", function () {
         });
 
         it("mint state", async function () {
-            await assetMintState({
+            await assertMany({
                 pool,
-                token0,
-                token1
+                token0: weth,
+                token1: usdc
             },{
                 poolBalance0: poolBalance.poolBalance0,
                 poolBalance1: poolBalance.poolBalance1,
@@ -168,10 +213,10 @@ describe("UniswapV3Pool mint tests", function () {
         });
 
         it("mint state", async function () {
-            await assetMintState({
+            await assertMany({
                 pool,
-                token0,
-                token1
+                token0: weth,
+                token1: usdc
             },{
                 poolBalance0: poolBalance.poolBalance0,
                 poolBalance1: poolBalance.poolBalance1,
@@ -216,10 +261,10 @@ describe("UniswapV3Pool mint tests", function () {
         });
 
         it("mint state", async function () {
-            await assetMintState({
+            await assertMany({
                 pool,
-                token0,
-                token1
+                token0: weth,
+                token1: usdc
             },{
                 poolBalance0: poolBalance.poolBalance0,
                 poolBalance1: poolBalance.poolBalance1,
@@ -235,6 +280,28 @@ describe("UniswapV3Pool mint tests", function () {
 
 })
 
+async function mintParamsToTicks(mintParams, currentPrice) {
+    const result = [];
+
+    const liq = liquidity(mintParams, currentPrice);
+
+    result.push({
+        tick: mintParams.lowerTick,
+        initialized: true,
+        liquidityGross: liq,
+        liquidityNet: liq
+    })
+
+    result.push({
+        tick: mintParams.upper,
+        initialized: true,
+        liquidityGross: liq,
+        liquidityNet: -liq
+    })
+
+    return result;
+}
+
 async function liquidity(mintParams, currentPrice) {
     return await testUtils.getLiquidityForAmounts(
         await testUtils.sqrtP(currentPrice),
@@ -246,12 +313,14 @@ async function liquidity(mintParams, currentPrice) {
 }
 
 async function mintParams(lowerPrice, upperPrice, amount0, amount1) {
-    const lowerTick = await testUtils.tick(lowerPrice);
-    const upperTick = await testUtils.tick(upperPrice);
+    const lowerTick = await testUtils.tick60(lowerPrice);
+    const upperTick = await testUtils.tick60(upperPrice);
     return {
-        poolAddress: "0x0",
-        lowerTick,
-        upperTick,
+        token0Address: wethAddress,
+        token1Address: usdcAddress,
+        fee: 3000,
+        lowerTick: lowerTick,
+        upperTick: upperTick,
         amount0Desired: parseEther(amount0),
         amount1Desired: parseEther(amount1),
         amount0Min: 0,
@@ -263,47 +332,49 @@ async function setup(params) {
     signers = await ethers.getSigners();
     accounts = signers.map((x) => x.address);
     ownerSigner = signers[0];
-    ownerAccount = accounts[0];
+    ownerAddress = accounts[0];
     lpSigner = signers[5];
-    lpAccount = accounts[5];
+    lpAddress = accounts[5];
 
     // 部署WETH和USDC合约
-    token0 = await ethers.deployContract("ERC20Mintable", ["Ether", "WETH", ownerAccount]);
-    token1 = await ethers.deployContract("ERC20Mintable", ["USDC", "USDC", ownerAccount]);
-    token0Account = token0.target;
-    token1Account = token1.target;
+    usdc = await ethers.deployContract("ERC20Mintable", ["USDC", "USDC", ownerAddress]);
+    weth = await ethers.deployContract("ERC20Mintable", ["Ether", "WETH", ownerAddress]);
+    usdcAddress = usdc.target;
+    wethAddress = weth.target;
 
-    // 初始化lp和trader的token余额
-    await token0.mint(lpAccount, params.lpToken0Balance);
-    await token1.mint(lpAccount, params.lpToken1Balance);
+    factory = await ethers.deployContract("UniswapV3Factory");
+    factoryAddress = factory.target;
 
-    // 部署WETH/USDC池子合约
-    const currentSqrtP = await testUtils.sqrtP(params.currentPrice);
-    const currentTick = await testUtils.tick(params.currentPrice);
-    pool = await ethers.deployContract("UniswapV3Pool",
-        [token0Account, token1Account, currentSqrtP, currentTick]);
-    poolAccount = pool.target;
+    await factory.createPool(wethAddress, usdcAddress, 3000);
+    const poolCreatedEvents = await factory.queryFilter(factory.filters.PoolCreated, 0);
+    wethUsdcPoolAddress = poolCreatedEvents[0].args[3];
 
-    // 部署管理合约
-    manager = await ethers.deployContract("UniswapV3Manager");
-    managerAccount = manager.target;
+    const { abi } = JSON.parse(require("fs").readFileSync(poolArtifactLocation).toString());
+    wethUsdcPool = new ethers.Contract(wethUsdcPoolAddress, abi, ethers.provider);
+    await wethUsdcPool.connect(lpSigner).initialize(await testUtils.sqrtP(5000));
 
-    // lp和trader批准manager一定数量的token
-    await token0.connect(lpSigner).approve(managerAccount, params.lpToken0Balance);
-    await token1.connect(lpSigner).approve(managerAccount, params.lpToken1Balance);
+    await weth.mint(lpAddress, params.lpToken0Balance);
+    await usdc.mint(lpAddress, params.lpToken1Balance);
+
+    manager = await ethers.deployContract("UniswapV3Manager", [factoryAddress]);
+    managerAddress = manager.target;
+
+    await weth.connect(lpSigner).approve(managerAddress, params.lpToken0Balance);
+    await usdc.connect(lpSigner).approve(managerAddress, params.lpToken1Balance);
 
     // 提供流动性
     const mintParamsArr = params.mintParamsArr;
     for (let i = 0; i < mintParamsArr.length; i++) {
         const mintParams = mintParamsArr[i];
-        mintParams.poolAddress = poolAccount;
+        mintParams.token0Address = wethAddress;
+        mintParams.token1Address = usdcAddress;
         await manager.connect(lpSigner).mint(mintParams);
     }
 
     let poolBalance0 = BigInt(0);
     let poolBalance1 = BigInt(0);
-    const filter = pool.filters.Mint;
-    const events = await pool.queryFilter(filter, 0);
+    const filter = wethUsdcPool.filters.Mint;
+    const events = await wethUsdcPool.queryFilter(filter, 0);
     events.forEach((e) => {
         poolBalance0 += e.args[e.args.length - 2]
         poolBalance1 += e.args[e.args.length - 1]
@@ -312,50 +383,7 @@ async function setup(params) {
     return {poolBalance0, poolBalance1};
 }
 
-
-async function assetMintState(contracts, expected) {
-    // pool's balance0
-    assert.equal(
-        await contracts.token0.balanceOf(poolAccount),
-        expected.poolBalance0
-    );
-
-    // pool's balance1
-    assert.equal(
-        await contracts.token1.balanceOf(poolAccount),
-        expected.poolBalance1
-    )
-
-    // pool's position liquidity
-    const positionKey = keccak256(solidityPacked(
-        ["address", "int24", "int24"],
-        [lpAccount, expected.lowerTick, expected.upperTick]
-    ));
-    const positionLiquidity = await contracts.pool.positions(positionKey);
-    assert.equal(positionLiquidity, expected.positionLiquidity);
-
-    // lower tick info
-    const lowTickInfo = await contracts.pool.ticks(expected.lowerTick);
-    assert.equal(lowTickInfo[0], true);
-    assert.equal(lowTickInfo[1], expected.positionLiquidity);
-    assert.equal(lowTickInfo[2], expected.positionLiquidity);
-
-    // upper tick info
-    const upperTickInfo = await contracts.pool.ticks(expected.upperTick);
-    assert.equal(upperTickInfo[0], true);
-    assert.equal(upperTickInfo[1], expected.positionLiquidity);
-    assert.equal(upperTickInfo[2], -expected.positionLiquidity);
-
-    // tick bit map
-    assert.equal(
-        await tickBitmap(contracts.pool, expected.lowerTick),
-        true
-    );
-    assert.equal(
-        await tickBitmap(contracts.pool, expected.upperTick),
-        true
-    );
-
+async function assertPoolState(contracts, expected) {
     // slot0
     const slot0 = await contracts.pool.slot0();
     assert.equal(
@@ -367,13 +395,106 @@ async function assetMintState(contracts, expected) {
         expected.tick
     );
 
-    // current liquidity
     assert.equal(
-        await contracts.pool.liquidity(),
-        expected.currentLiquidity
+        await contracts.pool.feeGrowthGlobal0X128(),
+        expected.fees[0]
+    );
+
+    assert.equal(
+        await contracts.pool.feeGrowthGlobal1X128(),
+        expected.fees[1]
     );
 }
 
+async function assertBalances(contracts, expected) {
+    assert.equal(
+        await contracts.token0.balanceOf(wethUsdcPoolAddress),
+        expected.poolBalance0
+    );
+    assert.equal(
+        await contracts.token1.balanceOf(wethUsdcPoolAddress),
+        expected.poolBalance1
+    );
+    assert.equal(
+        await contracts.token0.balanceOf(lpAddress),
+        expected.userBalance0
+    );
+    assert.equal(
+        await contracts.token1.balanceOf(lpAddress),
+        expected.userBalance1
+    );
+}
+
+async function assertTick(contracts, expected) {
+    const tick = await contracts.pool.ticks(expected.tick);
+
+    assert.equal(
+        tick[0],
+        expected.initialized
+    )
+
+    assert.equal(
+        tick[1],
+        expected.liquidityGross
+    )
+
+    assert.equal(
+        tick[2],
+        expected.liquidityNet
+    )
+}
+
+async function assertPosition(contracts, expected) {
+    const positionKey = keccak256(solidityPacked(
+        ["address", "int24", "int24"],
+        [lpAddress, expected.lowerTick, expected.upperTick]
+    ));
+    const position = await contracts.pool.positions(positionKey);
+
+    assert.equal(
+        position[0],
+        expected.liquidity
+    )
+
+    assert.equal(
+        position[1],
+        expected.feeGrowthInside0LastX128
+    )
+
+    assert.equal(
+        position[2],
+        expected.feeGrowthInside1LastX128
+    )
+
+    assert.equal(
+        position[3],
+        expected.tokensOwed0
+    )
+
+    assert.equal(
+        position[4],
+        expected.tokensOwed1
+    )
+}
+
+async function assertObservation(contracts, expected) {
+    const observation = await contracts.pool.observations(expected.index);
+
+    // assert.equal(
+    //     observation[0],
+    //     block.timestamp
+    // )
+
+    assert.equal(
+        observation[1],
+        expected.tickCumulative
+    )
+
+    assert.equal(
+        observation[2],
+        expected.initialized
+    )
+}
 
 async function tickBitmap(pool, tick) {
     const wordPos = tick >> 8n;
